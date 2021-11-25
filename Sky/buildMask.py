17b5b3e47fb9614f3cc5d8b108df79e76f74e5a9
David Castro <david.castro@mines-paristech.fr>
2021-11-26 00:57:50 +0100

    Représentation

--- Sky/buildMask.py
+++ Sky/buildMask.py
@@ -7,6 +7,7 @@ from algorithms.lda import LDA
 from algorithms.random import RandomClassifier
 from algorithms.qda import QDA
 from algorithms.kernels import Kernels
+from algorithms.logit import Logit
 
 def build(model_name, arg=None):
 
@@ -22,21 +23,39 @@ def build(model_name, arg=None):
         model = QDA()
     elif model_name == 'kernels':
         model = Kernels(arg) # arg is the expression of the kernel here (thus a function)
+    elif model_name == 'logit':
+        model = Logit()
     else:
         raise NameError('Model not found')
 
     model.train(train)
 
-    # Choix d'une image
+    # Choix d'une image et calcul du skymask
 
     dirs = [path for path in os.listdir('./Data') if path.startswith('ima')]
     src = np.random.choice(dirs)
     img = plt.imread('./Data/' + src)
     s = img.shape
     col = img.reshape((s[0]*s[1], 3))
-    skymask = model.predict(col).reshape(s)
+    skymask = model.predict(col).reshape((s[0], s[1]))
 
-    plt.imshow(img)
+    # Représentation
+
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+    ax1.imshow(img)
+    ax1.set_title('Image couleur')
+    ax1.set_axis_off()
+
     ref = plt.imread('./Data/mask' + src[3:-4] + '_skymask.png')
-    plt.imshow(ref, cmap='binary')
-    plt.imshow(skymask, cmap='binary')
+    ax2.imshow(ref, cmap='binary')
+    ax2.set_title('Skymask obtenu à la main')
+    ax2.set_axis_off()
+
+    ax3.imshow(skymask, cmap='binary')
+    ax3.set_title('Skymask calculé avec le model ' + model_name)
+    ax3.set_axis_off()
+
+    fig.tight_layout()
+    plt.show()
+
+build('QDA')
