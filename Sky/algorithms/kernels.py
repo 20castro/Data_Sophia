40d551ade62c9c05134ed8a7885f075328efd8b3
David Castro <david.castro@mines-paristech.fr>
2021-11-26 16:34:40 +0100

    Optimisation noyaux

--- Sky/algorithms/kernels.py
+++ Sky/algorithms/kernels.py
@@ -1,14 +1,13 @@
 import numpy as np
 from scores import Scores
-import time
 
+class GaussianKernel:
 
-class Kernels:
-
-    def __init__(self, K):
-        self.K = K
+    def __init__(self):
         self.class1 = None
         self.class0 = None
+        self.n1 = 0
+        self.n0 = 0
 
     def __repr__(self) -> str:
         return f'Modèle entraîné : {self.trained}'
@@ -18,19 +17,42 @@ class Kernels:
         mask0 = trainSet[:, 3] == 0
         self.class1 = trainSet[mask1, :3]
         self.class0 = trainSet[mask0, :3]
+        self.n1 = np.power(np.linalg.norm(self.class1, axis=1), 2).reshape((1, self.class1.shape[0]))
+        self.n0 = np.power(np.linalg.norm(self.class0, axis=1), 2).reshape((1, self.class0.shape[0]))
         self.trained = True
 
-    def predict(self, X): # à vectoriser (suppose ici que X est un vecteur)
-        D1 = X - self.class1
-        D0 = X - self.class0
-        N1 = self.K(D1).sum()
-        N0 = self.K(D0).sum()
-        return N0 > N1 # équivalent à N0/(N0 + N1) > 1/2
+    def predict(self, X):
+
+        ## Version générale vectorisée (avec self.K en argument de __init__, le noyau)
+        ## Très très lent
+        
+        # D1 = X.reshape((s[0], 1, s[1])) - self.class1
+        # D0 = X.reshape((s[0], 1, s[1])) - self.class0
+        # N1 = self.K(D1).sum(axis=1)
+        # N0 = self.K(D0).sum(axis=1)
+
+        ## Version non vectorisée (avec self.K en argument de __init__, le noyau)
+        ## Très lent
+
+        # N1 = np.zeros(s[0])
+        # N0 = np.zeros(s[0])
+        # for k, px in enumerate(X):
+        #     N1[k] += self.K(px - self.class1).sum()
+        #     N0[k] += self.K(px - self.class0).sum()
+
+        ## Dans les deux cas correspondant, on prendrait pour self.K :
+        # def gaussian(u):
+        #     return np.exp(-.5*np.power(np.linalg.norm(u, axis=len(u.shape) - 1), 2))/np.sqrt(2*np.pi)
+        
+        ## Version vectorisée (cas particulier adopté : gaussien mais généralisable facilement)
+        ## On décompose le calcul de la norme et on ne prend que les valeurs non constantes (on enlève ||x||^2)
+
+        N1 = np.exp(- X@np.transpose(self.class1) - self.n1).sum(axis=1)
+        N0 = np.exp(- X@np.transpose(self.class0) - self.n0).sum(axis=1)
+
+        return N1 > N0 # équivalent à N1/(N0 + N1) > 1/2
 
     def performance(self, testSet):
-        start = time.time()
-        pred = self.predict(testSet[:, :3])
-        end = time.time()
-        sc = Scores(testSet[:, 3], pred)
+        sc = Scores(testSet[:, 3], self.predict(testSet[:, :3]))
         print(sc)
-        return end - start
+        return sc
